771757a735c5d5972678b05c176e9a3a591de8ae
tamara.dzhikiya <djandzh.77@gmail.com>
2024-12-04 21:50:33 +0300

    footer

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -3,14 +3,12 @@ import { Outlet } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import NavBar from './ui/NavBar';
 import Loader from './HOCs/Loader';
-import Footer from './ui/Footer';
 export default function Layout({ user, logoutHandler }) {
   return (
     <Loader showSpinner={user.status === 'fetching'}>
       <Container fluid>
         <NavBar user={user} logoutHandler={logoutHandler} />
         <Outlet />
-        <Footer />
       </Container>
     </Loader>
   );
