89e46564772ab2f0f5c71d1b14e9982a4d01b6eb
Grossyles <epic.nikolaus@gmail.com>
2024-12-05 18:22:36 +0300

    Traslate Cards

--- client/src/components/ui/ItemCard.jsx
+++ client/src/components/ui/ItemCard.jsx
@@ -2,45 +2,43 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
+import '../CSS/CardItem.css'; 
 export default function ItemCard({ card }) {
-
-  const [show, setShow] = useState(false)
+  const [show, setShow] = useState(false);
 
   return (
     <>
-      <Card style={{
-        width: '350px',
-        height: '350px',
-        cursor: 'pointer',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'flex-end'
-      }}
-      onClick={() => setShow((prev) => !prev)}
-       >
-        <Card.Body 
+      <Card
+        style={{
+          width: '350px',
+          height: '350px',
+          cursor: 'pointer',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'flex-end',
+        }}
+        onClick={() => setShow((prev) => !prev)}
+      >
+        <Card.Body
           style={{
-            fontFamily: 'serif',
-            fontSize: ' 20px',
-          borderRadius: '10px',
-          position: 'absolute',
-          left: '50%',
-          bottom: '50%',
-          transform: 'translate(-50%, 33%)',
-          textAlign: 'center',
-          width: '100%',
-          padding: '10px',
-        }}>
-  
-          
-                  
-          <Card.Text>
-            {show ? card.engWord : card.rusWord}</Card.Text>
+            fontFamily: 'Press Start 2P, cursive', 
+            fontSize: '20px',
+            borderRadius: '10px',
+            position: 'absolute',
+            left: '50%',
+            bottom: '50%',
+            transform: 'translate(-50%, 33%)',
+            textAlign: 'center',
+            width: '100%',
+            padding: '10px',
+          }}
+        >
+          <Card.Text className='coolfont'>
+            {show ? card.engWord : card.rusWord}
+          </Card.Text>
         </Card.Body>
-      
-      <Button style={{
 
-      }}>X</Button>
+        <Button style={{}}>X</Button>
       </Card>
     </>
   );
